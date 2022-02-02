1596640cda0004bbc750ceff7d5c0086e31166bc
Jordan Del Colle <jdelcolle1@nait.ca>
2022-02-01 17:24:57 -0700

    Initial commit

---
+++ src/main/java/ca/nait/dmit/dmit20151212oe01jpademo/HelloApplication.java
@@ -0,0 +1,9 @@
+package ca.nait.dmit.dmit20151212oe01jpademo;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
